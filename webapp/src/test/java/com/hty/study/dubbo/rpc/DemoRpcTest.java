cb0a65f1afc453a27d6fd4379a5b027c2eaed1a6
韩同阳 <hantongyang@douyu.tv>
2017-09-21 20:51:52 +0800

    修改配置

--- webapp/src/test/java/com/hty/study/dubbo/rpc/DemoRpcTest.java
+++ webapp/src/test/java/com/hty/study/dubbo/rpc/DemoRpcTest.java
@@ -2,9 +2,13 @@ package com.hty.study.dubbo.rpc;
 
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
+@RunWith(SpringJUnit4ClassRunner.class)//表示整合JUnit4进行测试
+@ContextConfiguration(locations={"classpath:spring-config.xml"})//加载spring配置文件
 public class DemoRpcTest {
 
     @Autowired
