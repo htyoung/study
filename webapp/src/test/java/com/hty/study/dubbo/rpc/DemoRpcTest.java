541e95ceaf4c71ec70e7f95e6c65aa77ea716200
htyoung <hty551020@hotmail.com>
2017-09-21 15:43:29 +0800

    fixbug

--- webapp/src/test/java/com/hty/study/dubbo/rpc/DemoRpcTest.java
+++ webapp/src/test/java/com/hty/study/dubbo/rpc/DemoRpcTest.java
@@ -2,14 +2,9 @@ package com.hty.study.dubbo.rpc;
 
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:spring-config.xml"})
+
 public class DemoRpcTest {
 
     @Autowired
